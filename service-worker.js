4cbd78ea025e1ddb50d1cfc8220fe56423a29a4b
LST <suri2008us@gmail.com>
2026-06-04 10:31:08 +0530

    Add files via upload
    Spring Boot tutorials
    REST API lessons
    JPA/Hibernate basics
    Spring Security/JWT
    Microservices topics
    Spring Boot code examples
    Interview practice

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME="learning-app-v5";
+const CACHE_NAME="learning-app-v8";
 const FILES=["./","./index.html","./manifest.json","./service-worker.js","./icon.svg"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))))});
 self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
