18ab461a24f80df11dd48537345631029ff406ee
LST <suri2008us@gmail.com>
2026-06-04 17:00:41 +0530

    Add files via upload
    English Vocabulary
    English Grammar
    Java Tutorial
    Spring Boot Tutorials
    Selenium Tutorials
    Interview Practice
    Notes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="learning-app-v8";
+const CACHE_NAME="learning-app-v9";
 const FILES=["./","./index.html","./manifest.json","./service-worker.js","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))))});
